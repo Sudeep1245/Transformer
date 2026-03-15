bb957a4ce194ea38b7fd941e8e79b9f3f3aed008
Sudeep1245 <sudeep123sharma123@gmail.com>
2026-03-15 10:22:11 +0530

    added FNN

--- transformer_component.py
+++ transformer_component.py
@@ -1,5 +1,6 @@
 import torch
-
+import torch.nn as nn
+import torch.optim as optim
 
 class Tranformer_com:
 
@@ -62,6 +63,29 @@ class Tranformer_com:
         
         else :
             raise f'Please check you n_head ,n_head should completly divide the the column of input vector'
+        
+    def add_norm(self,input_data,masked_self_output):
+        residual_add = masked_self_output + input_data
+        return torch.layer_norm(residual_add)
+    
+class FeedForwardNN(nn.Module):
+
+    def __init__(self,input_size,hidden_size,output_size):
+        super(FeedForwardNN,self).__init__()
+
+        self.fc1 = nn.Linear(input_size,hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size,output_size)
+
+    def forward(self,x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+
+
 
 
                 
