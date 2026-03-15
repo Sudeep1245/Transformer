727c20dbb2628d5e20c05743e897271daecdea31
Sudeep1245 <sudeep123sharma123@gmail.com>
2026-03-15 09:58:48 +0530

    added masked function

---
+++ transformer_component.py
@@ -0,0 +1,67 @@
+import torch
+
+
+class Tranformer_com:
+
+    def softmax(self,mat):
+        mat = torch.exp(mat - torch.max(mat,axis=1,keepdim=True))
+        return mat/torch.sum(mat,axis=1,keepdim=True)
+
+    def masked_matrix(self,mat):
+        for row in range(mat.shape[0]):
+            for col in range(mat.shape[1]):
+                if col > row:
+                    mat[row][col] = -torch.inf
+                else :
+                    mat[row][col] = 0
+
+        return mat 
+
+    def masked_multi_head_attention(self,input_data,n_head):
+        d_k = input_data.shape[1]
+
+
+        if d_k%n_head == 0:
+            W_o = torch.random.rand(d_k,d_k,required_grad=True)
+
+            head_dict = {}
+            list_Iq = []
+            list_Ik = []
+            list_Iv = []
+
+            for head in range(n_head):
+                Q = torch.random.rand(d_k,d_k//n_head,required_grad=True)
+                K = torch.random.rand(d_k,d_k//n_head,required_grad = True)
+                V = torch.random.rand(d_k,d_k//n_head,required_grad = True)
+                list_Iq.append(Q),list_Ik.append(K),list_Iv.append(V)
+
+            head_dict['Iq'] = list_Iq
+            head_dict['Ik'] = list_Ik
+            head_dict['Iv'] = list_Iv
+
+            mask_input_list = []
+            for head in range(n_head):
+                Iq = torch.matmul(input_data,head_dict['Iq'][head])
+                Ik = torch.matmul(input_data,head_dict['Ik'][head])
+                Iv = torch.matmul(input_data,head_dict['Iv'][head])
+
+
+                score = (Iq@Ik.T)/torch.sqrt(d_k//n_head)
+
+                mat_for_mask = torch.ones(score.shape[0],score.shape[1])
+
+                masked_score = score + self.masked_matrix(mat_for_mask)
+
+                softmax_score = self.softmax(masked_score)
+
+                mask_input_list.append(softmax_score@Iv)
+
+                concat_head = torch.hstack(mask_input_list)
+
+            return torch.matmul(concat_head,W_o)
+        
+        else :
+            raise f'Please check you n_head ,n_head should completly divide the the column of input vector'
+
+
+                
